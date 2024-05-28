27e0dde22c8bcab3af61164571e506cce1da9873
Adam Cieślewicz <acadamus199@wp.pl>
2024-05-28 12:05:54 +0200

    seventh_commit

--- src/components/ApiFetch/Modal.tsx
+++ src/components/ApiFetch/Modal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Mod = {
   children: any,
   imageModal?: string,
@@ -10,6 +12,9 @@ type Mod = {
 };
 
 const Modal: React.FC<Mod> = ({ children, imageModal, title, artist, price, priceCurrency, itemCount, releaseDate }) => {
+  const [show, setShow] = useState<any>(title.length > 20 ? title.slice(0, 20) + ".." : title);
+  const [art, setArt] = useState<any>(artist.length > 15 ? artist?.slice(0, 15) + ".." : artist)
+  const [divid, setDivid] = useState<string>("divider")
 
   return (
     <>
@@ -19,7 +24,9 @@ const Modal: React.FC<Mod> = ({ children, imageModal, title, artist, price, pric
       <div className="modal" role="dialog">
         <div className="modal-box truncate">
           <center><img className="lg:w-80" src={imageModal} alt="" /></center>
-          <div className="divider text-sm sm:text-lg font-semibol truncate">{title.length > 20 ? title.slice(0, 20) + ".." : title} - {artist.length > 15 ? artist?.slice(0, 15) + ".." : artist}</div>
+          <center>
+            <div onMouseEnter={() => { setShow(title); setArt(artist); setDivid("h-4 mt-3 mb-4") }} onMouseOut={() => { setShow(title.length > 20 ? title.slice(0, 20) + ".." : title); setArt(artist.length > 15 ? artist?.slice(0, 15) + ".." : artist); setDivid("divider") }} className={`${divid} text-sm sm:text-lg font-semibol hover:animate-marquee`}>{show} - {art}</div>
+          </center>
           <p className="py-4 text-sm sm:text-lg">Count: {itemCount} / {price} {priceCurrency}</p>
           <p>Date: {releaseDate}</p>
           <div className="modal-action">
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,12 @@ export default {
       }
     },
     animation: {
-      marquee: 'marquee 30s linear infinite',
-      marquee2: 'marquee2 30s linear infinite'
+      marquee: 'marquee 10s linear infinite',
     },
     keyframes: {
       marquee: {
-        '0%': { transform: 'translateX(0%)' },
-        '100%': { transform: 'translateX(-100%)' }
+        '100%': { transform: 'translate(-100%, 0%)' }
       },
-      marquee2: {
-        '0%': { transform: 'translateX(100%)' },
-        '100%': { transform: 'translateX(0%)' }
-      }
     },
   },
   plugins: [
